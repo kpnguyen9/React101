6836f8c5532a40508f00fbdd6f99915e1e310e6d
kpnguyen9 <kpnguyen2@gmail.com>
2022-04-28 20:11:20 -0500

    completed blog

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>The Dogo Blog</h1>
+      <h1>Kevin's Blog</h1>
       <div className="links">
         <Link to="/">Home </Link>
         <Link to="/create">Create New Blog</Link>
